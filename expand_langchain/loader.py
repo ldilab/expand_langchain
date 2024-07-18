cb0feff919fa5b9e98f6a1afbd1637b47c6d2c3a
Jaejin Kim <jaejin.kim@snu.ac.kr>
2024-07-18 08:27:38 +0000

    add yaml support & delete embedding index feature

--- expand_langchain/loader.py
+++ expand_langchain/loader.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 
 import psycopg2
+import yaml
 from datasets import Dataset, load_dataset
 from expand_langchain.config import Config, DatasetConfig, SourceConfig
 from pydantic import BaseModel
@@ -52,6 +53,12 @@ class Loader(BaseModel):
                 data_json = json.loads(Path(path).read_text())
                 sources[name] = Dataset.from_list(data_json).sort(sort_key)
 
+            elif source.type == "yaml":
+                path = source.kwargs.get("path")
+                sort_key = source.kwargs.get("sort_key")
+                data_yaml = yaml.load(Path(path).read_text(), Loader=yaml.FullLoader)
+                sources[name] = Dataset.from_list(data_yaml).sort(sort_key)
+
             elif source.type == "postgresql":
                 sources[name] = psycopg2.connect(
                     dbname=source.kwargs.get("dbname"),
@@ -74,9 +81,6 @@ class Loader(BaseModel):
             if dataset.type == "dict":
                 datasets[name] = _load_dict(sources, **dataset.kwargs)
 
-            elif dataset.type == "index":
-                datasets[name] = _load_index(sources, **dataset.kwargs)
-
             elif dataset.type == "schema":
                 datasets[name] = _load_schema(sources, **dataset.kwargs)
 
@@ -98,145 +102,6 @@ def _load_dict(sources, primary_key, fields):
             result[id][field.get("name")] = source[i][field.get("key")]
 
     return result
-
-
-def _load_index(sources_dict, mode, sources, path, model, platform):
-    if platform == "open_webui":
-        embedding_function = OllamaEmbeddings(
-            model=model,
-            base_url=os.environ["OPEN_WEBUI_BASE_URL"],
-            headers={
-                "Authorization": f"Bearer {os.environ['OPEN_WEBUI_API_KEY']}",
-                "Content-Type": "application/json",
-            },
-        )
-    else:
-        raise ValueError(f"Unknown platform: {platform}")
-
-    client = chromadb.PersistentClient(path)
-
-    results = {}
-    for key in sources:
-        try:
-            client.get_collection(key)
-        except ValueError:
-            collection = client.create_collection(key)
-            conn = sources_dict[key]
-            values = defaultdict(list)
-            if mode == "entity":
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        SELECT
-                            n.nspname AS schema,
-                            c.relname AS table,
-                            a.attname AS column,
-                            pg_catalog.obj_description(c.oid, 'pg_class') AS table_description,
-                            pg_catalog.col_description(a.attrelid, a.attnum) AS column_description
-                        FROM
-                            pg_catalog.pg_class c
-                        JOIN
-                            pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                        JOIN
-                            pg_catalog.pg_attribute a ON a.attrelid = c.oid
-                        WHERE
-                            c.relkind = 'r' -- only tables
-                            AND a.attnum > 0
-                            AND NOT a.attisdropped
-                            AND n.nspname NOT IN ('pg_catalog', 'information_schema')
-                        ORDER BY
-                            n.nspname, c.relname, a.attnum;"""
-                    )
-                    output = cur.fetchall()
-
-                    for (
-                        schema,
-                        table,
-                        column,
-                        table_description,
-                        column_description,
-                    ) in output:
-                        cur.execute(
-                            f"SELECT DISTINCT {column} FROM {schema}.{table} LIMIT 20;"
-                        )
-                        rows = cur.fetchall()
-                        for row in rows:
-                            values[row[0]].append(
-                                {
-                                    "table": table,
-                                    "column": column,
-                                    "table_description": table_description,
-                                    "column_description": column_description,
-                                }
-                            )
-            elif mode == "context":
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        SELECT
-                            n.nspname AS schema,
-                            c.relname AS table,
-                            pg_catalog.obj_description(c.oid, 'pg_class') AS description
-                        FROM
-                            pg_catalog.pg_class c
-                        JOIN
-                            pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                        WHERE
-                            c.relkind = 'r' -- only tables
-                            AND n.nspname NOT IN ('pg_catalog', 'information_schema')
-                        ORDER BY
-                            n.nspname, c.relname;"""
-                    )
-                    output = cur.fetchall()
-                    for schema, table, description in output:
-                        values[description].append({"table": f"{schema}.{table}"})
-
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        SELECT
-                            n.nspname AS schema,
-                            c.relname AS table,
-                            a.attname AS column,
-                            pg_catalog.col_description(a.attrelid, a.attnum) AS description
-                        FROM
-                            pg_catalog.pg_class c
-                        JOIN
-                            pg_catalog.pg_namespace n ON n.oid = c.relnamespace
-                        JOIN
-                            pg_catalog.pg_attribute a ON a.attrelid = c.oid
-                        WHERE
-                            c.relkind = 'r' -- only tables
-                            AND a.attnum > 0
-                            AND NOT a.attisdropped
-                            AND n.nspname NOT IN ('pg_catalog', 'information_schema')
-                        ORDER BY
-                            n.nspname, c.relname, a.attnum;"""
-                    )
-                    output = cur.fetchall()
-                    for schema, table, column, description in output:
-                        values[description].append(
-                            {
-                                "table": f"{schema}.{table}",
-                                "column": column,
-                            }
-                        )
-            else:
-                raise ValueError(f"Unknown mode: {mode}")
-
-            collection.add(
-                ids=list(map(str, range(len(values)))),
-                documents=list(values.keys()),
-                metadatas=list(values.values()),
-            )
-
-        results[key] = Chroma(
-            client=client,
-            collection_name=key,
-            embedding_function=embedding_function,
-        )
-
-    return results
 
 
 def _load_schema(sources_dict, sources):
